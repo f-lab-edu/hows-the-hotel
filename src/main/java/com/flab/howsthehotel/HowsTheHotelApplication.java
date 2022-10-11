3adaa2d11d0a60c6c16358a0053af5639ae590c2
hyelim <vkfks4472889@gmail.com>
2022-10-11 13:28:46 +0900

    first commit

---
+++ src/main/java/com/flab/howsthehotel/HowsTheHotelApplication.java
@@ -0,0 +1,13 @@
+package com.flab.howsthehotel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowsTheHotelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowsTheHotelApplication.class, args);
+    }
+
+}
